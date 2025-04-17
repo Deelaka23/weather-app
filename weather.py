816911bae92cd65a1b5f78c57395f83dd5602285
Deelaka Rishan <deelakanelumdeniye@gmail.com>
2025-04-17 19:23:51 +0530

    Weather
    Learn how to build a real-time **Weather App using Python** and the **OpenWeatherMap API**! 🌦️ Perfect for beginners who want to practice using APIs and the `requests` library in Python.
    
    In this tutorial, you'll learn:
    ✅ How to install the `requests` library  
    ✅ How to fetch live weather data using API  
    ✅ How to parse and display the weather info (city, country, temperature, wind, and more)

---
+++ weather.py
@@ -0,0 +1,27 @@
+import requests
+
+#Ask for a city name
+city_name = input("Enter the City name:  ".capitalize())
+
+# Define your API key
+api = "8b67c922030522929bc7c01153a47c14"
+
+# Make the API request
+s_request = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api}&units=metric"
+)
+s_request = s_request.json()  # Convert JSON response to dictionary
+
+# Extract relevant data
+country = s_request['sys']['country']  
+city = s_request['name']
+temp = s_request['main']['temp']
+wind = s_request['wind']['speed']
+weather = s_request['weather'][0]['description']
+
+# Print results
+print(f"Country name  : {country}")
+print(f"City name     : {city}")
+print(f"Temperature   : {temp} °C")
+print(f"Wind speed    : {wind} km/h")
+print(f"Weather       : {weather.capitalize()}")
